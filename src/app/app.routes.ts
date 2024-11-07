8e7a26760178234462cfca7b188a683527014a3b
vitor-pavanello-nextia <vitor.pavanello@nextia.com.br>
2024-11-07 20:55:50 -0300

    alteraçao caminho de pastas / estilização tela consultar paciente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { RealizarAgendamentoComponent } from './modules/telemedicina/realizar-agendamento/realizar-agendamento.component';
-import { ConsultarAgendamentoComponent } from './modules/telemedicina/consultar-agendamento/consultar-agendamento.component';
+import { RealizarAgendamentoComponent } from './modules/telemedicina/pages/realizar-agendamento/realizar-agendamento.component';
+import { ConsultarAgendamentoComponent } from './modules/telemedicina/pages/consultar-agendamento/consultar-agendamento.component';
 import { CadastrarPacienteComponent } from './modules/paciente/pages/cadastrar-paciente/cadastrar-paciente.component';
 import { EditarPacienteComponent } from './modules/paciente/pages/editar-paciente/editar-paciente.component';
 import { ConsultarPacienteComponent } from './modules/paciente/pages/consultar-paciente/consultar-paciente.component';
@@ -13,11 +13,10 @@ export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'realizar-agendamento', component: RealizarAgendamentoComponent },
+  { path: 'realizar-agendamento/:id', component: RealizarAgendamentoComponent },
   { path: 'consultar-agendamento', component: ConsultarAgendamentoComponent },
   { path: 'cadastrar-paciente', component: CadastrarPacienteComponent },
   { path: 'editar-paciente/:id', component: EditarPacienteComponent },
   { path: 'consultar-paciente', component: ConsultarPacienteComponent },
   { path: 'visualizar-prontuario/:id', component: VisualizarProntuarioComponent },
-
 ];
