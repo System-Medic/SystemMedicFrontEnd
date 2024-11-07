c4058eabf486876d225ddc11ca9ecff4bd040f4a
vitor-pavanello-nextia <vitor.pavanello@nextia.com.br>
2024-11-07 14:44:41 -0300

    ajustes sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SystemFrontend';
+  showSidebar: boolean = true;
+
+  constructor(private router: Router) {
+    // Verifica a rota atual para esconder a navbar na tela de login
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.showSidebar = !this.router.url.includes('/login');  // Oculta a sidebar na página de login
+    });
+  }
 }
